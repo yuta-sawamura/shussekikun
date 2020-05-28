0abbe1c059bccf8f18872b3599bab33e00c7274f
Yuuta Sawamura <>
2020-05-28 09:24:18 +0900

    [add]マイグレーションファイルを追加

---
+++ server/database/migrations/2020_05_27_234255_create_classes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('organization_id')->comment('組織ID');
+            $table->foreign('organization_id')->references('id')->on('organizations');
+            $table->string('name', 50)->comment('クラス名');
+            $table->smallInteger('day')->comment('曜日(1:日 2:月 3:火 4:水 5:木 6:金 7:土)');
+            $table->time('start_at')->comment('開始時間');
+            $table->time('end_at')->comment('終了時間');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
---
+++ server/database/migrations/2020_05_28_002126_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('organization_id')->comment('組織ID');
+            $table->foreign('organization_id')->references('id')->on('organizations')->comment('組織ID');
+            $table->string('name', 50)->comment('カテゴリー名');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
