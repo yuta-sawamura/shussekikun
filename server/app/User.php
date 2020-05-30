c0d635f5d89e2185314ab86373328f25be013e49
Yuuta Sawamura <>
2020-05-30 14:44:38 +0900

    [update]モデル定数を追加

--- server/app/User.php
+++ server/app/User.php
@@ -36,4 +36,26 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    const GENDER_MAN = 1;
+    const GENDER_WOMAN = 2;
+    const GENDER_LIST = [
+        slef::GENDER_MAN => '男',
+        slef::GENDER_WOMAN => '女',
+    ];
+
+    const ROLE_ORGANIZATION_ADMIN = 3;
+    const ROLE_SHARE = 5;
+    const ROLE_LIST = [
+        slef::ROLE_ORGANIZATION_ADMIN => '組織管理者',
+        slef::ROLE_SHARE => '共有アカウント',
+    ];
+
+    const STATUS_CONTINUE = 1;
+    const STATUS_CANCEL = 2;
+    const STATUS_LISt = [
+        self::STATUS_CONTINUE => '継続',
+        self::STATUS_CANCEL => '退会',
+    ];
+
 }
