1c480dd5ae3ff7809906c2a7b5158a926d077bb7
Yuuta Sawamura <>
2020-07-01 08:27:26 +0900

    [update]アイコンの変更

--- server/resources/views/layouts/admin/_sidebar.blade.php
+++ server/resources/views/layouts/admin/_sidebar.blade.php
@@ -15,10 +15,6 @@
       <li class="menu active">
         <a href="#dashboard" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
           <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
-              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-              <polyline points="9 22 9 12 15 12 15 22"></polyline>
-            </svg>
             <span>ダッシュボード</span>
           </div>
           <div>
@@ -44,9 +40,9 @@
         <a href="{{ url('admin/user/aggregate') }}" aria-expanded="false" class="dropdown-toggle">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2">
-            <line x1="18" y1="20" x2="18" y2="10"></line>
-            <line x1="12" y1="20" x2="12" y2="4"></line>
-            <line x1="6" y1="20" x2="6" y2="14"></line>
+              <line x1="18" y1="20" x2="18" y2="10"></line>
+              <line x1="12" y1="20" x2="12" y2="4"></line>
+              <line x1="6" y1="20" x2="6" y2="14"></line>
             </svg>
             <span>ホーム</span>
           </div>
@@ -98,6 +94,20 @@
               <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
               <line x1="12" y1="8" x2="12" y2="16"></line>
               <line x1="8" y1="12" x2="16" y2="12"></line>
+            </svg>
+            <span class="badge badge-success"></span>
+            <span>クラス</span>
+          </div>
+        </a>
+      </li>
+      <li class="menu">
+        <a href="{{ url('admin/schedule') }}" aria-expanded="false" class="dropdown-toggle">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
+              <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+              <line x1="16" y1="2" x2="16" y2="6"></line>
+              <line x1="8" y1="2" x2="8" y2="6"></line>
+              <line x1="3" y1="10" x2="21" y2="10"></line>
             </svg>
             <span class="badge badge-success"></span>
             <span>スケジュール</span>
