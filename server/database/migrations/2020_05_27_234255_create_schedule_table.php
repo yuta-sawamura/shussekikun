1ed45a3420912e27bd90ae54e166dfb9f107b2e5
Yuuta Sawamura <>
2020-07-01 08:42:06 +0900

    [fix]テーブル構成の誤りを修正
    クラスとスケジュールの関係が逆だった

--- server/database/migrations/2020_05_27_234255_create_schedule_table.php
+++ server/database/migrations/2020_05_27_234255_create_schedule_table.php
@@ -17,6 +17,8 @@ class CreateScheduleTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('store_id')->comment('店舗ID');
             $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
+            $table->unsignedBigInteger('classwork_id')->comment('クラスID');
+            $table->foreign('classwork_id')->references('id')->on('classworks')->onDelete('cascade');
             $table->unsignedSmallInteger('day')->comment('曜日(1:日 2:月 3:火 4:水 5:木 6:金 7:土)');
             $table->time('start_at')->comment('開始時間');
             $table->time('end_at')->comment('終了時間');
