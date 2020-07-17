d7b7acbbebf39fd1f6ec0ee7d9eef7c77b8f0ddf
Yuuta Sawamura <>
2020-07-18 08:28:36 +0900

    [delete]サイドバーの不要コード削除

--- server/resources/views/layouts/admin/_sidebar.blade.php
+++ server/resources/views/layouts/admin/_sidebar.blade.php
@@ -12,30 +12,6 @@
     </ul>
     <div class="shadow-bottom"></div>
     <ul class="list-unstyled menu-categories" id="accordionExample">
-      <li class="menu active">
-        <a href="#dashboard" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
-          <div>
-            <span>ダッシュボード</span>
-          </div>
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right">
-              <polyline points="9 18 15 12 9 6"></polyline>
-            </svg>
-          </div>
-        </a>
-        <ul class="collapse submenu recent-submenu mini-recent-submenu list-unstyled show" id="dashboard" data-parent="#accordionExample">
-          <li>
-            <a href="{{ url('admin/user/show/' . Auth::id()) }}"> マイページ </a>
-          </li>
-          <li>
-            <form method="post" name="sidebar_logout" action="{{ route('logout') }}">
-              @csrf
-              <input type="hidden" name="logout" value="">
-              <a href="javascript:sidebar_logout.submit()"> ログアウト </a>
-            </form>
-          </li>
-        </ul>
-      </li>
       <li class="menu">
         <a href="{{ url('admin/user/aggregate') }}" aria-expanded="false" class="dropdown-toggle">
           <div>
