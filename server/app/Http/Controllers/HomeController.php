8f911264bfc0f423778d1aa3af22802bd7d82cca
Yuuta Sawamura <>
2020-07-26 10:56:34 +0900

    [add]セレクトボックスのグループ化

--- server/app/Http/Controllers/HomeController.php
+++ server/app/Http/Controllers/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Models\Schedule;
+use Auth;
+use App\Enums\User\Role;
+use DB;
 
 class HomeController extends Controller
 {
@@ -15,13 +20,78 @@ class HomeController extends Controller
     {
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
+    public function index (Request $request)
     {
-        return view('home/index');
+        $params = $request->query();
+
+        if (Auth::check()) {
+            $users = User::where('store_id', Auth::user()->store_id)
+                ->where('role', Role::Normal)
+                ->serachKeyword($params['keyword'] ?? null)
+                ->storeFilter($params['store'] ?? null)
+                ->categoryFilter($params['category'] ?? null)
+                ->genderFilter($params['gender'] ?? null)
+                ->orderBy('id', 'desc')
+                ->paginate(30);
+
+    // $days = DB::table('schedules')
+    //     ->select(DB::raw(
+    //         "CASE day
+    //             WHEN 1 THEN
+    //                 '日曜日'
+    //             WHEN 2 THEN
+    //                 '月曜日'
+    //             WHEN 3 THEN
+    //                 '火曜日'
+    //             WHEN 4 THEN
+    //                 '水曜日'
+    //             WHEN 5 THEN
+    //                 '木曜日'
+    //             WHEN 6 THEN
+    //                 '金曜日'
+    //             WHEN 7 THEN
+    //                 '土曜日'
+    //             END AS day_name"
+    //     ))
+    //     ->where('store_id', Auth::user()->store_id)
+    //     ->groupBy('day_name')
+    //     ->pluck('day_name');
+
+        $schedules = Schedule::select(
+                'schedules.day',
+                'schedules.id',
+                DB::Raw("CONCAT(classworks.name, ' ', TIME_FORMAT(schedules.start_at, '%H:%i'), '~', TIME_FORMAT(schedules.end_at, '%H:%i')) as name")
+            )
+            ->join('classworks', 'classworks.id', '=', 'schedules.classwork_id')
+            ->where('schedules.store_id', Auth::user()->store_id)
+            ->get()
+            ->groupBy(function($schedule) {
+                $days = [
+                    '日曜日' => 1,
+                    '月曜日' => 2,
+                    '火曜日' => 3,
+                    '水曜日' => 4,
+                    '木曜日' => 5,
+                    '金曜日' => 6,
+                    '土曜日' => 7
+                ];
+                return array_search($schedule['day'], $days);
+            })
+            ->map(function ($items, $key) {
+                return $items->map(function ($item) {
+                    return [
+                        $item['id'] => $item['name']
+                    ];
+                });
+            });
+
+            //dd($schedules);
+    }
+
+        return view('home.index')->with([
+            'users' => $users ?? null,
+            'schedules' => $schedules ?? null,
+            'params' => $params,
+        ]);
     }
 }
--- server/resources/views/components/modals/attendance.blade.php
+++ server/resources/views/components/modals/attendance.blade.php
@@ -1,57 +1,39 @@
-<div class="modal fade" id="attendanceModal" tabindex="-1" role="dialog" aria-labelledby="attendanceModalLabel" aria-hidden="true">
+<div class="modal fade" id="attendanceModal{{ $user->id }}" tabindex="-1" role="dialog" aria-labelledby="attendanceModal" aria-modal="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item d-flex align-items-center">
-            澤村 勇太
-            <button type="button" class="close" aria-label="閉じる">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle">
-                <circle cx="12" cy="12" r="10"></circle>
-                <line x1="15" y1="9" x2="9" y2="15"></line>
-                <line x1="9" y1="9" x2="15" y2="15"></line>
-              </svg>
-            </button>
-          </li>
-          <li class="list-group-item d-flex align-items-center">
-            澤村 勇太
-            <button type="button" class="close" aria-label="閉じる">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle">
-                <circle cx="12" cy="12" r="10"></circle>
-                <line x1="15" y1="9" x2="9" y2="15"></line>
-                <line x1="9" y1="9" x2="15" y2="15"></line>
-              </svg>
-            </button>
-          </li>
-        </ul>
+        <h5 class="modal-title" id="exampleModalLabel">出席</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
-            <line x1="18" y1="6" x2="6" y2="18"></line>
-            <line x1="6" y1="6" x2="18" y2="18"></line>
-          </svg>
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
         </button>
       </div>
       <div class="modal-body">
-        <select class="form-control nested">
-          <optgroup label="月曜日">
-            <option selected="selected">朝クラス</option>
-            <option>昼クラス</option>
-            <option>夜クラス</option>
-          </optgroup>
-          <optgroup label="水曜日">
-            <option>月曜日</option>
-            <option>水曜日</option>
-            <option>金曜日</option>
-          </optgroup>
-          <optgroup label="金曜日">
-            <option>月曜日</option>
-            <option>水曜日</option>
-            <option>金曜日</option>
-          </optgroup>
-        </select>
+        <ul>
+          <li class="mb-2">
+            <p>{{ $user->full_name }}</p>
+          </li>
+        </ul>
+        <div class="form-group">
+          <label>スケジュール<span class="text-danger">*</span></label>
+          <div class="row">
+            <div class="col-md-12">
+              <select class="form-control" name="category_id" required>
+                <option selected="selected" value="">選択してください</option>
+                @foreach($categories as $k => $v)
+                  <option value="{{ $k }}">
+                    {{ $v }}
+                  </option>
+                @endforeach
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          {{ Form::select('schedule', $schedules) }}
+        </div>
       </div>
       <div class="modal-footer">
-        <button class="btn" data-dismiss="modal"><i class="flaticon-cancel-12"></i> キャンセル</button>
+        <button class="btn" data-dismiss="modal"><i class="flaticon-cancel-12"></i>キャンセル</button>
         <button type="button" class="btn btn-primary">出席</button>
       </div>
     </div>
