08f965ac29cc1709df890afe30c4d176f359b41c
Yuuta Sawamura <>
2020-08-09 21:19:17 +0900

    [fix]サイドバー、ルーティングの修正

--- server/resources/views/layouts/admin/_sidebar.blade.php
+++ server/resources/views/layouts/admin/_sidebar.blade.php
@@ -13,7 +13,7 @@
     <div class="shadow-bottom"></div>
     <ul class="list-unstyled menu-categories" id="accordionExample">
       <li class="menu">
-        <a href="{{ url('admin/user/aggregate') }}" aria-expanded="false" class="dropdown-toggle">
+        <a href="{{ url('admin/') }}" aria-expanded="false" class="dropdown-toggle">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2">
               <line x1="18" y1="20" x2="18" y2="10"></line>
