920efdc0984cf78cba9dced8cfefe8f2f2e027f0
Yuuta Sawamura <>
2020-09-26 09:26:38 +0900

    [update]ビューにrequiredの付与、バリデーションのリダイレクト時の考慮

--- resources/views/admin/news/_form.blade.php
+++ resources/views/admin/news/_form.blade.php
@@ -2,10 +2,10 @@
   @csrf
   <div class="form-group col-xl-4 col-md-12 col-sm-12 col-12">
     <label>店舗<span class="text-danger">*</span></label>
-    <select class="form-control @error('store_id') is-invalid @enderror" name="store_id">
+    <select class="form-control @error('store_id') is-invalid @enderror" name="store_id" required>
       <option selected="selected" value="">選択してください</option>
       @foreach($stores as $k => $v)
-        <option value="{{ $k }}" {{ isset($news->store_id) && $news->store_id == $k ? 'selected': '' }}>
+        <option value="{{ $k }}" {{ old('store_id') == $k || isset($news->store_id) && $news->store_id == $k ? 'selected': '' }}>
           {{ $v }}
         </option>
       @endforeach
@@ -15,15 +15,14 @@
   </div>
   <div class="form-group col-xl-4 col-md-12 col-sm-12 col-12">
     <label>タイトル<span class="text-danger">*</span></label>
-    <input type="text" name="title" value="{{ $news->title ?? null }}" class="form-control @error('title') is-invalid @enderror">
+    <input type="text" name="title" value="{{ $errors->has('*') ? old('title'):($news->title ?? '') }}" class="form-control @error('title') is-invalid @enderror" required>
     @include('components.validations.feedback', ['message' => 'title'])
 
   </div>
   <div class="form-group col-xl-10 col-md-12 col-sm-12 col-12">
     <label>本文<span class="text-danger">*</span></label>
-    <textarea name="content" id="news-content">{{ $news->content ?? null }}</textarea>
+    <textarea name="content" id="news-content">{{ $errors->has('*') ? old('content'):($news->content ?? '') }}</textarea>
     @include('components.validations.feedback', ['message' => 'content'])
-
   </div>
   <div class="col-xl-10 col-md-12 col-sm-12 col-12 text-right">
     @isset ($isEdit)
